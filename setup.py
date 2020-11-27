55224989713e59d06889be8cf990ed5e2dd088b9
heetbeet <sfstreicher@gmail.com>
2020-11-27 16:39:23 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="locate",
     version="0.0.1",
-    author="Simon Streicher",
+    author="Heetbeet",
     author_email="sfstreicher@gmail.com",
     description="Locate the file location of your current running script.",
     long_description=long_description,
