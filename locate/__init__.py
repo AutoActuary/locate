a62b91bd04bfb23ffa758e3afa333e32da5cb605
Bart-Jan Hulsman <hulsmanbj@gmail.com>
2022-03-30 09:27:15 +0200

    Layout improvement.

--- locate/__init__.py
+++ locate/__init__.py
@@ -1,1 +1,7 @@
-from .locate import this_dir, allow_relative_location_imports, force_relative_location_imports
+from .locate import (
+    this_dir,
+    allow_relative_location_imports,
+    force_relative_location_imports,
+    append_sys_path,
+    prepend_sys_path,
+)
